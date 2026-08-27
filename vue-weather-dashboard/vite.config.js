ebfe13e3f1f79629e5d4a054980428ef60d6f29f
이송미 <dlthdal07@gmail.com>
2026-08-27 17:07:25 +0900

    🔧 refactor: vite 설정 변경

--- vue-weather-dashboard/vite.config.js
+++ vue-weather-dashboard/vite.config.js
@@ -15,4 +15,11 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: 'dist',
+  },
 })
